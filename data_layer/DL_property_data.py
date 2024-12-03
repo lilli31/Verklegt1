1ca0866b24f005df4cee7dcbfa7cd4e18b8c0737
rakeleir <rakelm24@ru.is>
2024-12-03 23:29:08 +0000

    prop DL meira

--- data_layer/DL_property_data.py
+++ data_layer/DL_property_data.py
@@ -12,29 +12,44 @@ class Property:
     def __str__(self) -> str:
         return f"{self.destination_id}, {self.property_id}, {self.property}, {self.address}, {self.rental_space} "
         
+    def __format__(self, format_spec: str) -> str:
+        return f"{self.destination_id},{self.property_id},{self.property},{self.address},{self.rental_space}"
+
 
 
 class DL_Properties():
     def __init__(self):
-        self.filepath = '/Users/rakeleir/Desktop/Untitled/data_files/properties.csv'
+        self.filepath = '/Users/rakeleir/Desktop/Untitled/data_files/properties.csv' #breyta þetta virkar bara fyrir mig
 
-    def store_properties(self, file_path):
-        """Load properties from a CSV file into the dictionary """
-    
+    def store_properties(self, file_path): #actually hvað
+        pass
+
     def fetch_properties(self) -> list[Property]: 
-        print(os.listdir)
         with open(self.filepath, 'r') as file:
             rows = csv.reader(file, delimiter='\n')
             properties = self.__create_models(rows)
             return properties
-            
+    
+    def fetch_property(self, property_id):
+        pass
             
 
-    def add_properties(self):
-        pass
-    def delete_properties(self):   
-        pass
-    def update_properties(self):
+    def add_properties(self, property: Property):
+        with open(self.filepath, 'a') as file:
+            propertyString = format(property)
+            file.write(propertyString) #það sem ég vil setja inn 
+
+    def delete_property(self, property: Property):   
+        with open(self.filepath, 'r+') as file:
+            rows = file.readlines()
+            file.seek(0)
+            for property_to_delete in rows:    
+                if property_to_delete.strip() != format(property): #if property is not the one we want to delete we keep it by writing
+                    file.write(property_to_delete)
+                #if it is the property we want to delete it will not be written and truncated from file
+                file.truncate()
+
+    def update_properties(self): #nota edit file eða??
         pass
     
     def __create_models(self,rows: list[list[str]]):
@@ -51,5 +66,7 @@ class DL_Properties():
 
 dl_properties = DL_Properties()
 properties = dl_properties.fetch_properties()
+#dl_properties.add_properties(properties[0])
+dl_properties.delete_property(properties[1])
 
 
