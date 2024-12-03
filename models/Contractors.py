ef0894e102cf6685badb02e25c0a0a2c266c3096
isold sigurjonsdottir <thorbjorgs24@ru.is>
2024-12-03 11:01:02 +0000

    modeles contractors

---
+++ models/Contractors.py
@@ -0,0 +1,15 @@
+class contractors:
+    def __init__(self):
+        self.contractor_id = None
+        self.name = None
+        self.company = None
+        self.phone = None
+        self.contact_name = None
+        self.opening = None
+        self.address = None
+        self.jobs = None
+
+
+
+
+    
