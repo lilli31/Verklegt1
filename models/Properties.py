67dca8c325e7fa7f0bf09954a469e703b5aa5a31
vigdisklara <vigdis24@ru.is>
2024-12-04 10:39:59 +0000

    models update

--- models/Properties.py
+++ models/Properties.py
@@ -1,9 +1,9 @@
 
 class Properties:
-    def __init__ (self):
-        self.destination_id = None 
-        self.property_id = None 
-        self.property = None 
-        self.address = None 
-        self.rental_space = None 
+    def __init__ (self, destination_ID: int = None, property_ID: str = None, property: str = None, address: str = None, rental_space: int = None):
+        self.destination_ID = destination_ID 
+        self.property_ID = property_ID
+        self.property = property
+        self.address = address
+        self.rental_space = rental_space 
 
--- models/Report.py
+++ models/Report.py
@@ -1,13 +1,13 @@
 class Reports:
-    def __init__(self):
-        self.report_ID = None
-        self.work_order_ID = None
-        self.work_done = None
-        self.contractor = None
-        self.contractor_ID = None
-        self.additional_report_info = None
-        self.material_cost = None
-        self.contractor_cost = None
-        self.total_cost = None
+    def __init__(self, report_id: int = None, work_order_ID: int = None, work_done: str = None, contractor: bool = None, contractor_id: int = None, additional_report_info: str = None, material_cost: int = None, contractor_cost: int = None, total_cost: int = None):
+        self.report_ID = report_id
+        self.work_order_ID = work_order_ID
+        self.work_done = work_done
+        self.contractor = contractor
+        self.contractor_ID = contractor_id
+        self.additional_report_info = additional_report_info
+        self.material_cost = material_cost
+        self.contractor_cost = contractor_cost
+        self.total_cost = total_cost
 
         
