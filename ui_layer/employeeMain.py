6c814ea287ea2d6b5d58779fa2b39078ae9fab31
reginasjofn <reginasjofn@gmail.com>
2024-12-04 15:51:29 +0000

    Main update-að

--- ui_layer/employeeMain.py
+++ ui_layer/employeeMain.py
@@ -3,22 +3,20 @@
 class EmployeeMain:
 
     def __init__(self, id_number, ll_wrapper):
-        self.id_number = id_number
-        self.employee_info = ll_wrapper.getEmployee(self.id_number)
-        self.job_title = self.employee_info[0]
-        self.name = self.employee_info[1]
-        self.displayEmployeeMain()
+        #self.id_number = id_number
+        employee_info = ll_wrapper.getEmployee(id_number)
+        self.job_title = employee_info[1]
+        self.name = employee_info[0]
+        #self.displayEmployeeMain()
 
     def displayEmployeeMain(self):
-        print("\n", ("-" * 70))
+        print("\n")
+        print("-" * 70)
         print("|",(self.name.ljust(33)), (self.job_title.rjust(32)), "|")
         print("-" * 70)
+        print("|", "My work-orders".ljust(30), "|", "My reports".ljust(33), "|")
+        print("-" * 70, "\n")
+        self.displayChoices()
 
-    def backToChoice(self):
-        pass
-
-    def viewMyWorkOrders(self):
-        pass
-
-    def viewMyReports(self):
-        pass
+    def displayChoices(self):
+        choice = input("1) View my work-orders\n2) View my reports\n\nq) Quit\n\nInput choice: ")
