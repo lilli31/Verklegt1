db320b86408aa92cde3cbbdf719ef84c01f3393b
rakeleir <rakelm24@ru.is>
2024-12-04 23:27:03 +0000

    LL pro pælingar

--- logic_layer/LL_property.py
+++ logic_layer/LL_property.py
@@ -1,9 +1,26 @@
 import csv
 import LL_property
+from data_layer.DL_properties_data import DL_Properties
+class LL_Property:
 
+    def __init__(self, dl_property: DL_Properties) -> None:
+        self.dl_property = dl_property
 
-    def verifyPropertyID():
-        pass
+    def verifyPropertyID(self, propertyID: str):
+        if len(propertyID)!= 5:
+             return False
+        if propertyID != propertyID.capitalize():
+             return False
+        return True #og meira
+    
+    def getPropertyByID(self,id: str):
+        if not self.verifyPropertyID(id):
+            return "Not valid ID"
+        properties = self.dl_property.FetchProperties()
+        for property in properties:
+            if property.property_ID == id:
+                return property
+        return False #búa til error?
 
     def verifyPropertyInfo():
         pass
@@ -17,5 +34,5 @@ import LL_property
     def getPropertyID():
         pass
 
-    def getAllProperties():
-        pass
+    def getAllProperties(self):
+        return self.dl_property.FetchProperties()
