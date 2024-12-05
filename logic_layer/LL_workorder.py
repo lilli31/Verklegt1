53594b3e317ae9fe15cdc9e80af1b4ae455020a5
vigdisklara <vigdis24@ru.is>
2024-12-05 10:30:48 +0000

    LL workorder update

--- logic_layer/LL_workorder.py
+++ logic_layer/LL_workorder.py
@@ -1,7 +1,24 @@
-class LL_WorkOrder:
+from data_layer.DL_WorkOrder_data import DL_WorkOrders
 
-    def verifyWorkOrderID():
-        pass
+class LL_WorkOrder:
+    def __init__(self, dl_workorder: DL_WorkOrders) -> None:
+        self.dl_workorder = dl_workorder
+
+    def verifyWorkOrderID(self, workorder_ID: int):
+        if workorder_ID != int:
+            return False
+        else:
+            return True
+        
+    def verifyNewWorkOrderID(self):
+        workorders = self.dl_workorder.FetchWorkOrders()
+        highestID = max(workorders)
+        for id in range(0, highestID+1):
+            if id <= highestID or id == 0:
+                for newid in range(0, highestID+1):
+                    if newid == id:
+                        continue
+                    
 
     def verifyWorkOrderInfo():
         pass
@@ -16,4 +33,8 @@ class LL_WorkOrder:
         pass
 
     def getWorkOrderID():
+        pass
+
+
+    def get_my_work_orders():
         pass
