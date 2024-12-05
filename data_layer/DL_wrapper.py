30aa64c7407e258b324bbe077c34c1561f312db9
reginasjofn <reginasjofn@gmail.com>
2024-12-05 14:24:10 +0000

    LL_reports get_my_reports

--- data_layer/DL_wrapper.py
+++ data_layer/DL_wrapper.py
@@ -1,11 +1,28 @@
 from data_layer.DL_employee_data import DL_Employees
+from data_layer.DL_WorkOrder_data import DL_WorkOrders
+from data_layer.DL_report_data import DL_Report
+from data_layer.DL_contractors_data import DL_Contractor
 
 class DataLayerWrapper:
 
     def __init__(self):
         self.dl_employee_instance = DL_Employees()
+        self.dl_work_orders = DL_WorkOrders()
+        self.dl_reports = DL_Report()
+        self.dl_contractors = DL_Contractor()
     
     def getAllEmployees(self) -> list:
+
         return self.dl_employee_instance.FetchEmployees()
     
+    def get_all_work_orders(self) -> list:
+
+        return self.dl_work_orders.FetchWorkOrders()
   
+    def get_all_reports(self) -> list:
+
+        return self.dl_reports.FetchReports()
+    
+    def get_all_contractors(self) -> list:
+
+        return self.dl_contractors.FetchContractors()
--- logic_layer/LL_report.py
+++ logic_layer/LL_report.py
@@ -1,4 +1,42 @@
+from models.Contractors import Contractors
+from models.Report import Reports
+
 class LL_Report:
+
+    def __init__(self):
+        self.m_reports = Reports()
+        self.m_contractors = Contractors()
+
+    def get_my_reports(self, id_num, dl_wrapper) -> list:
+
+        report_list = []
+        for report in self.m_reports:
+            if int(report.report_id) == id_num:
+                work_order_id = report.work_order_ID
+                work_done = report.work_done
+                contractor_id = report.contractor_id
+                additional_info = report.additional_report_info
+                material_cost = report.material_cost
+                contractor_cost = report.contractor_cost
+                total_cost = report.total_cost
+                if bool(contractor_id) == True:
+                    contractors = dl_wrapper.get_all_contractors()
+                    for contractor in contractors:
+                        if report.contractor_id == contractor.contractor_id:
+                            contractor_name = contractor.name
+                            contact_phone = contractor.contact_phone
+                            contact_name = contractor.contact_name
+                            specialty = contractor.specialty
+                            report_list.append([work_order_id, work_done, contractor_name, contact_phone, contact_name, specialty, additional_info, material_cost, contractor_cost, total_cost])
+                else:
+                    report_list.append([work_order_id, work_done, additional_info, material_cost, total_cost])
+
+        return report_list
+    
+    def sort_my_reports(self, reports: list) -> list:
+        pass
+
+
 
     def verifyReportID():
         pass
