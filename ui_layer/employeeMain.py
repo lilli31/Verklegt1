58a029293e66969ecdb36ec4215721f7e7fea139
reginasjofn <reginasjofn@gmail.com>
2024-12-05 10:37:17 +0000

    Update menu og wrapper update

--- ui_layer/employeeMain.py
+++ ui_layer/employeeMain.py
@@ -1,13 +1,14 @@
-
+# from ui_layer.mainMenu import MainMenu
 
 class EmployeeMain:
 
     def __init__(self, id_number, ll_wrapper):
-        #self.id_number = id_number
-        employee_info = ll_wrapper.getEmployee(id_number)
+        #self.main = MainMenu()
+        self.id_num = id_number
+        self.wrapper = ll_wrapper
+        employee_info = self.wrapper.getEmployee(self.id_num)
         self.job_title = employee_info[1]
         self.name = employee_info[0]
-        #self.displayEmployeeMain()
 
     def displayEmployeeMain(self, logo):
         print(f"\033[96m{logo}\033[0m")
@@ -20,5 +21,6 @@ class EmployeeMain:
         self.displayChoices()
 
     def displayChoices(self):
-        choice = input("1) View my work-orders\n2) View my reports\n\nq) Quit\n\nInput choice: ")
-        
+        choice = input("1) View my work-orders\n2) View my reports\n\nb) Back\n\nInput choice: ")
+        if choice == "1":
+            work_orders = self.wrapper.get_my_work_orders(self.id_num)
