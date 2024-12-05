855e14aa3226a6afb0be67f1db230fa100c828c7
vigdisklara <vigdis24@ru.is>
2024-12-05 15:49:30 +0000

    update LL_workorder

--- logic_layer/LL_workorder.py
+++ logic_layer/LL_workorder.py
@@ -1,16 +1,22 @@
-import csv
 from data_layer.DL_WorkOrder_data import DL_WorkOrders
+from data_layer.DL_wrapper import DataLayerWrapper
 
 class LL_WorkOrder:
-    def __init__(self, dl_workorder: DL_WorkOrders) -> None:
-        self.dl_workorder = dl_workorder
+    def __init__(self, dl_wrapper):
+        self.dl_wrapper = dl_wrapper
 
-    def verifyWorkOrderID(self, workorder_ID: int):
-        if workorder_ID != int:
-            return False
-        else:
-            return True
+    def verifyWorkOrderID(workorderID):
+
+        try:
+            workorder_ID = DL_WorkOrders.FetchWorkOrders()
+        except Exception as e:
+            return False  # Gera error message!!!
         
+        workorder_ID = [workorder["workorderID"] for workorder in workorder_ID]
+        return workorderID in workorder_ID
+        
+
+    """    
     def verifyNewWorkOrderID(self):
         workorders = self.dl_workorder.FetchWorkOrders()
         highestID = max(workorders)
@@ -28,7 +34,7 @@ class LL_WorkOrder:
         workorders.append(new_id)
         #print(f"New ID added: {new_id}")  # This will print the new ID - þarf þetta í LL?
         return workorders, new_id
-                    
+    """             
 
 def verifyWorkOrderInfo(self, Work_order_ID, Employee, Contractor, Contractor_ID, Property_IDs, Maintenance_info, Regular, Days_between_or_when, Visible, Opens, Finished, Approved, State_of_work_order, Priority):
     if not isinstance(Work_order_ID, int):
@@ -77,4 +83,3 @@ def getWorkOrderID():
 def get_my_work_orders(self, id_num):
     pass
 
-isinstance(int)
